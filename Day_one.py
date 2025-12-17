329ed08c424b37fa10cc9f322dabf3f3012d18dd
Shivam Prajapati <shivamp94012@gmail.com>
2025-12-17 19:00:53 +0530

    Add files via upload

---
+++ Day_one.py
@@ -0,0 +1,24 @@
+#day one ->->->->
+
+print ("Day One")
+
+#comments --------->>
+
+# print("shivam prajapati") --> This is sinlge line comments
+
+""" This is the multiline comments
+this can comments multiline
+use three double/single qoutes to comments multiline together. """
+
+# escape sequence --------->>
+
+# \n is used to next line
+# \" is used to qoutes the word in string 
+print("Welcome to the day one of the python programing \n my name is \"shivam prajapati\" \n Lets's start to learning python programing from basic to advance " )
+
+# print statement --------->>
+
+# sep='' is used to saperate object, if there is more than one
+# end='' is used to what to print at the end
+
+print("hello world", 25 , 6 , sep='~')
