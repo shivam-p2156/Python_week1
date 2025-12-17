4c83b60e4cbd37dca2deb3aa20f0eb13d2b95719
Shivam Prajapati <shivamp94012@gmail.com>
2025-12-18 01:16:44 +0530

    Add files via upload

---
+++ Day_two.py
@@ -0,0 +1,29 @@
+# Day 2 ------->>>>
+
+# Variable
+
+a = 10
+print(a)
+
+b= "Python"
+print(b)
+
+c= 25.06
+print(c)
+
+d= a+c
+print(d)
+
+#Dtata Type
+
+"""
+int = 10,20,30
+string = "shivam", "python"
+float = 10.5, 3.14
+boolean = True, False
+
+"""
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
