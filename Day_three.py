dc16f2fb7755d59cc9dea11f9b037cf63e7d33af
Shivam Prajapati <shivamp94012@gmail.com>
2025-12-19 15:52:31 +0530

    Add files via upload

---
+++ Day_three.py
@@ -0,0 +1,34 @@
+#operators ----->>>
+
+a = 10
+b = 5
+
+print(a+b)   #Addition
+print(a-b)   #Subtraction
+print(a*b)   #Multiplication
+print(a/b)   #Division
+
+c = 15
+d = 2
+
+print(c//d)  #Floor division
+print(c%d)   #Modulo
+
+
+# Type casting
+
+#Explicit Typecasting
+A = "15"
+B = "10"
+
+sum = int(A)+int(B)
+print(sum)
+
+#Implicit Typecasting
+
+num1 = 50
+num2 = 20.5
+
+sum2 = num1+num2
+print(sum2,type(sum2))
+
