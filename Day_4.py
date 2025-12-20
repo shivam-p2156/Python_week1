47351fc67877255fa271dcf370eb7ab27dfe119c
Shivam Prajapati <shivamp94012@gmail.com>
2025-12-20 20:13:05 +0530

    Update Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -44,5 +44,59 @@ print(name[:5]) #slicing from start
 print(name[0:]) #slicing till end
 print(name[-4:5]) #slicing using negative index
 
+#String Methods
 
+s1 = "abcdefghi"
+print(s1.upper()) #uppercase methods
+
+s2 = "ABCDEFGHI"
+print(s2.lower()) #lowercase methods
+
+s3 = " Heloo World "
+print(s3.strip())  #strip method
+
+s4 = "heloo world sss"
+print(s4.rstrip("s")) #rstrip method
+
+s5 = "silver spoon"
+print(s5.replace("sp","m"))  #replace method 
+
+s6 = "silver spoon"
+print(s6.split())  #split methods
+
+s7 = "silver spoon"
+print(s7.capitalize())  #capitlize methods
+
+print(s7.center(15))  #center methods
+
+print(s7.count("s"))  #count methods
+
+print(s7.endswith("spoon"))  #endswith methods
+
+print(s7.find("oo"))  #find methods
+
+print(s7.index("spoon"))  #index methods
+
+s8 = "helloworld"
+print(s8.isalnum())  #isalnum methods
+
+print(s8.isalpha())  #isalpha methods
+
+s9 = "HELOO WORLD"
+print(s9.isupper())  #isupper methods
+
+s10 = "heloo world"
+print(s10.islower()) #islower methods
+
+print(s9.isprintable()) #isprintable methods
+
+print(s10.isspace())  #isspace methods
+
+print(s3.istitle())  #istitle methods
+
+print(s10.startswith("heloo"))  #startwith methods
+
+print(s10.swapcase())  #swapcase methods
+
+print(s10.title())  #title methods
 
