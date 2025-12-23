805424cc96d56db818cfbe4f6dd1f1270a7d50e2
Shivam Prajapati <shivamp94012@gmail.com>
2025-12-23 15:58:46 +0530

    Add files via upload

---
+++ exersice_1_calculator.py
@@ -0,0 +1,15 @@
+a = int(input("enter your first number: "))
+b = int(input("enter your second number: "))
+
+c= int(input("enter operation :"))
+
+if (c==1):
+    print(a+b)
+elif (c==2):
+    print(a-b)
+elif (c==3):
+    print(a*b)
+elif (c==4):
+    print(a/b)
+else:
+    print("error")
